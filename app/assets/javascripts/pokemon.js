5bba2846b2e4eaf11561c417a3d618fc760cb23a
Belen Linacero <belenlinacero@gmail.com>
2016-11-24 12:02:20 +0100

    initial commit

--- app/assets/javascripts/pokemon.js
+++ app/assets/javascripts/pokemon.js
@@ -1,2 +1,31 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+PokemonApp.Pokemon = function(pokemonUri){
+  this.id = PokemonApp.idFromUri(pokemonUri);
+};
+PokemonApp.Pokemon.prototype.render= function () {
+  console.log("Rendering pokemon: #" + this.id);
+};
+PokemonApp.idFromUri = function(pokemonUri){
+  var uriSegments = pokemonUri.split("/");
+  var secondLast = uriSegments.length - 2;
+  return uriSegments[secondLast];
+}
+
+$(document).on("ready",function(){
+  $(".js-show-pokemon").on("click",function(event){
+    var $button = $(event.currentTarget);
+    var pokemonUri = $button.data("pokemon-uri");
+
+    var pokemon = new PokemonApp.Pokemon(pokemonUri);
+    pokemon.render();
+  });
+  $.ajax({
+    url: "/api/pokemon/" + this.id,
+    success: function(response){
+      console.log("Pokemon info:");
+      console.log(response);
+      $('.js-pokemon-modal').modal("show");
+    }
+  });
+});
