282f79cab1ed626179a18a233df075a67147458d
Belen Linacero <belenlinacero@gmail.com>
2016-11-25 19:51:31 +0100

    Evolutions and Images

--- app/assets/javascripts/pokemon.js
+++ app/assets/javascripts/pokemon.js
@@ -3,8 +3,21 @@
 PokemonApp.Pokemon = function(pokemonUri){
   this.id = PokemonApp.idFromUri(pokemonUri);
 };
+
+PokemonApp.PokemonEvolutions= function(evolutionuri,pokeoriginal){
+  this.id = PokemonApp.idFromUri(evolutionuri);
+  this.original = pokeoriginal;
+};
+
+PokemonApp.idFromUri = function(pokemonUri){
+  var uriSegments = pokemonUri.split("/");
+  var secondLast = uriSegments.length - 2;
+  return uriSegments[secondLast];
+};
+
 PokemonApp.Pokemon.prototype.render= function () {
   console.log("Rendering pokemon: #" + this.id);
+  var idPokemon = parseInt(this.id) + 1;  // el ultimo parámetro de la uri de la imagen del pokemon es igual a su id +1.
   $.ajax({
     url: "/api/pokemon/" + this.id,
     success: function(response){
@@ -20,21 +33,63 @@ PokemonApp.Pokemon.prototype.render= function () {
       $('.js-pkmn-sp-attack').text(response.sp_atk);
       $('.js-pkmn-sp-defense').text(response.sp_def);
       $('.js-pkmn-speed').text(response.speed);
-      var types= response.types;
-      for (var i = 0; i<types.length; i++){
-          $('.js-pkmn-types').text(types[i].name);
+      $('.js-pkmn-exp').text(response.exp);
+      $('.js-pkmn-sprites')
+      var evolutions= response.evolutions;
+      // $('.js-show-evolutions').empty();
+      console.log(evolutions.length);
+      if(evolutions.length === 0){
+        $('.js-show-evolutions').text("No Evolutions");
+      }else{
+        $('.js-show-evolutions').text("Show Evolutions");
+        console.log(evolutions[0].resource_uri);
+        $('.js-show-evolutions').attr('data-evolution-uri',evolutions[0].resource_uri);
+      }
+      var types=response.types;
+      $('.js-pkmn-types').empty();
+      for(var i=0; i<types.length; i++){
+          $('.js-pkmn-types').append(`<dd>${types[i].name}</dd>`).text()
       };
+      $.ajax({
+        url: "https://pokeapi.co/api/v1/sprite/" + idPokemon,
+        success: function(response){
+        console.log("Pokemon Image");
+        console.log(response);
+        if(response.image.length != 0){
+          var uriArray= response.image.split('/')
+          var imgUrl= uriArray[uriArray.length-1].split('.')[0];
+          debugger
+        }else{
+          "not available"
+        };
+        $('.js-show-image').attr('src','https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+imgUrl);
+        }
+      });
       $('.js-pokemon-modal').modal("show");
     }
   });
 };
 
-PokemonApp.idFromUri = function(pokemonUri){
-  var uriSegments = pokemonUri.split("/");
-  var secondLast = uriSegments.length - 2;
-  return uriSegments[secondLast];
+PokemonApp.PokemonEvolutions.prototype.render= function(){
+  console.log("Rendering pokemon evolutions: #" + this.id + " from: " + this.original);
+  $.ajax({
+    url: "api/pokemon/" + this.id,
+    success: function(response){
+      console.log("Evolutions info");
+      console.log(response);
+      $('.js-pkmn-evol-name').text(response.name);
+      $('.js-pkmn-evol-number').text(response.pkdx_id);
+      $('.js-pkmn-evol-height').text(response.height);
+      $('.js-pkmn-evol-weight').text(response.weight);
+      $('.js-pkmn-evol-hp').text(response.hp);
+      $('.js-pkmn-evol-attack').text(response.attack);
+      $('.js-pkmn-evol-defense').text(response.defense);
+      $('.js-pkmn-evol-speed').text(response.speed);
+      $('.js-pkmn-evol-exp').text(response.exp);
+      $('.js-evolutions-modal').modal("show");
+    }
+  });
 }
-
 $(document).on("ready",function(){
   $(".js-show-pokemon").on("click",function(event){
     var $button = $(event.currentTarget);
@@ -42,5 +97,12 @@ $(document).on("ready",function(){
 
     var pokemon = new PokemonApp.Pokemon(pokemonUri);
     pokemon.render();
+  });
+  $(".js-show-evolutions").on("click", function(event){
+    $('.js-pokemon-modal').modal('toggle');
+    var $button= $(event.currentTarget);
+    var evolutionUri=$button.data("evolution-uri");
+    var pokemonEvolutions= new PokemonApp.PokemonEvolutions(evolutionUri,$('.js-pkmn-name').text());
+    pokemonEvolutions.render();
   });
 });
